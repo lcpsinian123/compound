a836a2d34156f3ab15ce3c1a152998028b4d6bc8
lcpsinian123 <lcpsinian@163.com>
2019-11-07 14:27:53 +0800

    updata vuex

---
+++ src/components/vuex.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        this is vuex {{count}}
+        <div @click='add'> + </div>
+        <div @click='jj'> - </div>
+    </div>
+
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+    data(){
+        return {
+
+        }
+    },
+    computed:mapState({
+        count:state=>state.count
+
+    }),
+    methods: {
+        add(){
+            this.$store.commit('add',1)
+        },
+        jj(){
+            this.$store.commit('jl',1)
+        }
+    },
+}
+</script>
